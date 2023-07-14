443837314362d49a79dc51578287cdea1611f5cf
mayank-quantumbot <mayankchauhan.it@gmail.com>
2023-07-14 19:08:17 +0530

    completed auth

--- home/urls.py
+++ home/urls.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth.views import LogoutView
 from . import views
 
 
 urlpatterns = [
     path('', views.home, name="home"),
+    path('dashboard/', views.dashboard, name='dashboard'),
+    path('logout/', LogoutView.as_view(next_page='/'), name='logout'),
 ]
--- home/views.py
+++ home/views.py
@@ -1,16 +1,24 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
+from django.contrib.auth import authenticate, login
+
+
+def dashboard(request):
+    return render(request, 'home/dashboard.html')
+
 
 # Create your views here.
 def home(request):
-    User = 'admin'
-    Pass = 'admin'
-
-
     if request.method == 'POST':
         username = request.POST.get('user_name')
         password = request.POST.get('password_name')
-        print("Username:", username)
-        print("Password:", password)
+        user = authenticate(request, username=username, password=password)
 
+        if user is not None:
+            login(request, user)
+            # Redirect to a success page or do further processing
+            return redirect('dashboard')
+        else:
+            # Authentication failed, handle the error
+            return render(request, 'home/login.html', {'error': 'Invalid credentials'})
 
-    return render(request, 'home/home.html')
+    return render(request, 'home/login.html')
